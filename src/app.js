42cdf28c793984b4a9017c43114965492b7f663a
skumhar-genpact <subhash.kumhar@genpact.com>
2025-04-14 01:01:28 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import express from 'express'
 
 const app = express()
 
+app.get('/test', (req, res) => {
+  console.log('req:', req?.query) // {userId:12}
+  res.send(`Hello World ftom test route: ${JSON.stringify(req?.query)}`)
+})
+app.get('/dashboard/:userId/:name/:password', (req, res) => {
+  console.log('req:', req?.params) // {userId:12, name:'Subhash',password:'Password@123'}
+  res.send(`Hello World dashboard: ${JSON.stringify(req?.params)}`)
+})
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-app.get('/test', (req, res) => {
-    res.send('Hello World ftom test route')
-  })
-  app.get('/dashboard', (req, res) => {
-    res.send('Hello World dashboard')
-  })
 
 app.listen(3000)
